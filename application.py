e8451364447435521ac4bbefe41a88c501035301
weiran <liweiran97@gmail.com>
2020-01-28 12:37:23 +0000

    Weiran test commit

--- application.py
+++ application.py
@@ -1,14 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return 'hello world  supervisor gunicorn '
+
 @app.route('/1')
 def index1():
     return 'hello world  supervisor gunicorn  ffffff'
+
 @app.route('/qw/1')
 def indexqw():
     return 'hello world  supervisor gunicorn fdfdfbdfbfb '
+
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
