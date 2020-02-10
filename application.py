25ca78713dc24914bcdd8812dd7fad8c23b868b0
weiran <liweiran97@gmail.com>
2020-02-10 11:06:30 +0000

    Add basic homepage

--- application.py
+++ application.py
@@ -1,9 +1,41 @@
 from flask import Flask, render_template, flash, redirect, url_for, sessions, request
-app = Flask(__name__)
+from wtforms import Form, StringField, validators, ValidationError, SelectField
 
-@app.route('/')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456'
+
+
+class SearchForm(Form):
+    content = StringField('Content', [
+        validators.Length(min=1, max=20, message=u'Search content should less than 20')
+    ])
+
+    type = SelectField('Type', choices=[('Type 1', 'Type 1'), ('Type 2', 'Type 2')])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    form = SearchForm(request.form)
+    if request.method == 'POST' and form.validate():
+        content = form.content.data
+        c_type = form.type.data
+        return redirect(url_for('search', content=content, c_type=c_type))
     return render_template('home.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    form = SearchForm(request.form)
+    if request.method == 'POST' and form.validate():
+        content = form.content.data
+        c_type = form.type.data
+        return content + c_type
+
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
 
 
 if __name__ == '__main__':
