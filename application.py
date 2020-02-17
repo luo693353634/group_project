effcd5a2311c4bfeab164ab4374943d7fd7fe6dd
ZIJIAN LUO <49781953+luo693353634@users.noreply.github.com>
2020-02-17 17:59:16 +0000

    baseline

---
+++ application.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, flash, redirect, url_for, sessions, request
+from wtforms import Form, StringField, validators, ValidationError, SelectField
+import algorithm
+from flask_pymongo import PyMongo
+from search import *
+
+MODE = mode['product']  # test or product
+
+
+
+app = Flask(__name__)
+# app.config['SECRET_KEY'] = '123456'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/firstdb"
+# app.config.update(
+#     MONGO_HOST='localhost',
+#     MONGO_PORT=27017,
+#     # MONGO_USERNAME='ttdspj',
+#     # MONGO_PASSWORD='123456',
+#     MONGO_DBNAME='flask'
+# )
+mongo = PyMongo(app)
+
+
+class SearchForm(Form):
+    content = StringField('Content', [
+        validators.Length(min=1, max=20, message=u'Search content should less than 20')
+    ])
+
+    type = SelectField('Type', choices=[('Type 1', 'Type 1'), ('Type 2', 'Type 2')])
+
+
+index_for_search = load_pickle(pickle_index_p[MODE])  # load index
+tokens_id = load_pickle(tokens_id_vb_p[MODE])
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    form = SearchForm(request.form)
+    if request.method == 'POST' and form.validate():
+        content = form.content.data
+        c_type = form.type.data
+        """
+        将查询的词传入算法，然后返回查询结果。查询结果应当使用SQL或者字典以加快运行速度
+        如果涉及到属性，应定义一个类，规范化检索过程
+        """
+        query, phrase = preprocess_query(content)
+        result_list = final_search(query, phrase, index_for_search, tokens_id)
+
+        # result_list = final_search(query, phrase, index_for_search, tokens_id)
+        # search_result = algorithm.tf_idf(content)
+        # result_list = algorithm.searching_alghrithm(content)
+
+        search_result = []
+        for item in result_list:
+            # search_result.append(mongo.db.song_poem.find_one({"doc_id": int(item)}))
+            search_result.append(mongo.db.song_poem.find_one({"doc_id": decode_vbyte(item)}))
+            if len(search_result) >= 20:
+                break
+
+        return render_template('search.html', content=content, c_type=c_type, search_result=search_result)
+
+
+@app.route('/result/<int:result_id>')
+def result(result_id):
+    res = mongo.db.song_poem.find_one({"doc_id": int(result_id)})
+    return render_template('result.html', result=res)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ baseline/tools.py
@@ -0,0 +1,104 @@
+# -*- coding=utf-8 -*
+import json
+import re
+import string
+import jieba
+import operator
+import pickle
+from preprocess import stopping
+from compress import *
+import os
+
+
+def write_pickle(dicc, out_file):
+    df2 = open(out_file, 'a')
+    df2.seek(0)
+    df2.truncate()
+    df2.close()
+    df2 = open(out_file, 'wb')
+    pickle.dump(dicc, df2)
+    df2.close()
+
+
+def write_json(path, dic_w):
+    f = open(path, 'a')
+    f.seek(0)
+    f.truncate()
+    json.dump(json.dumps(dic_w), f)
+    f.close()
+
+
+def write_txt(path, dic_w):
+    f = open(path, 'a')
+    f.seek(0)
+    f.truncate()
+    f.write(str(dic_w))
+    f.close()
+
+
+def load_json(path):
+    reader = json.loads(json.load(open(path, 'r')))
+    # f = open(path, 'r+')
+    # reader = json.loads(f.read())
+    return reader
+
+
+def load_pickle(path):
+    df = open(path, 'rb')
+    reader = pickle.load(df)
+    return reader
+
+
+def load_txt_dic(index_p):
+    with open(index_p, 'r') as f:
+        return eval(f.read())
+
+
+def load_tokenid(path='data/token2id'):
+    tokens_id = dict()
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            line = line.split()
+            tokens_id[line[1]] = encode_vbyte(int(line[0]))
+    return tokens_id
+
+
+def load_tokensid_vb(path='data/token2id_vb'):
+    tokens_id = (path)
+    return tokens_id
+
+
+def sort_dic_key(dic):
+    res = sorted(dic.items(), key=operator.itemgetter(0))
+    return res  # format [(0, 1), (2, 1), (5, 1)]
+
+
+def sort_dic_value(dic):
+    res = sorted(dic.items(), key=operator.itemgetter(1))
+    return res
+
+
+if __name__ == '__main__':
+    dicc = {b'\x80': 14, b'\x81': {b'\x81': [1], b'\x86': [213]},
+            b'\x82': {b'\x81': [2], b'\x8e': [1]}, b'\x83': {b'\x81': [3]},
+            b'\x84': {b'\x81': [4]}, b'\x85': {b'\x81': [5]},
+            b'\x86': {b'\x81': [6]}, b'\x87': {b'\x81': [7]},
+            b'\x88': {b'\x82': [1]}, b'\x89': {b'\x82': [2]},
+            b'\x8a': {b'\x82': [3]}, b'\x8b': {b'\x83': [1], b'\x84': [1]}, b'\x8c': {b'\x83': [2]},
+            b'\x8d': {b'\x83': [4, 20, 5]}, b'\x8e': {b'\x83': [5]},
+            b'\x8f': {b'\x83': [7]}, b'\x90': {b'\x83': [8, 23], b'\x84': [30]},
+            b'\x91': {b'\x83': [10, 25], b'\x86': [43, 196, 44, 317]},
+            b'\x92': {b'\x83': [11], b'\x84': [12]}, b'\x93': {b'\x83': [13]}, b'\x94': {b'\x83': [14]},
+            b'\x95': {b'\x83': [16, 16, 18, 27], b'\x84': [31]},
+            b'\x96': {b'\x83': [17, 30]}, b'\x97': {b'\x83': [19, 1], b'\x84': [14, 1]},
+            b'\x98': {b'\x83': [21]}, b'\x99': {b'\x83': [22], b'\x84': [2, 15]},
+            b'\x9a': {b'\x83': [26], b'\x85': [14], b'\x86': [152, 137, 162, 148, 171, 169, 251, 179], b'\x88': [26]},
+            b'\x9b': {b'\x83': [27], b'\x84': [5, 17]}, b'\x9c': {b'\x83': [29]}}
+    df2 = open('test.pickle', 'wb')
+    pickle.dump(dicc, df2)
+    df2.close()
+    df = open('test.pickle', 'rb')  # 注意此处是rb
+    # 此处使用的是load(目标文件)
+    data3 = pickle.load(df)
+    print(data3[b'\x9b'])
+
